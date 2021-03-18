4cab7a3cd254a23faec621a697b8ebade1890589
Samuel Rodrigues <samuelclerod@gmail.com>
2021-03-18 19:20:03 -0300

    change link

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://unijuazeiro.edu.br"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Viste a unijuazeiro
         </a>
       </header>
     </div>
