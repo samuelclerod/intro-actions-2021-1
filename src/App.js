37ead0dfb3103ac59fdeb486c29cd17f0d38c8f0
Samuel Rodrigues <samuelclerod@gmail.com>
2021-03-18 19:48:33 -0300

    test fixed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 function App() {
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Viste a unijuazeiro
+          Visite a unijuazeiro
         </a>
       </header>
     </div>
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders unijuazeiro link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/visite a unijuazeiro/i);
   expect(linkElement).toBeInTheDocument();
 });
