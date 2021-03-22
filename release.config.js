f0b0303fb61850e9f0b32994ee0b6659aa3ae2ad
Samuel Rodrigues <samuelclerod@gmail.com>
2021-03-22 19:52:10 -0300

    feat: add semantic release

---
+++ release.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  branches: "master",
+  repositoryUrl: "https://github.com/samuelclerod/intro-actions-2021-1",
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/github"
+  ]
+}
